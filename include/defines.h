1ae62c7f3e1a8c801482a527e593f714f667643e
nilswag <nilswagemaker05@icloud.com>
2025-07-21 16:25:10 +0200

    Added defines header

---
+++ include/defines.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdint.h>
+
+typedef _Bool bool;
+#define true  1
+#define false 0
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+#define STMT(_) do { _; } while (true)
+
+#ifdef _WIN32
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#else
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
