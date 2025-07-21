cec0544d0de6d05af092319e37c9f5fa82d5fc6c
nilswag <nilswagemaker05@icloud.com>
2025-07-21 21:32:31 +0200

    Refactored window code into main function

--- src/main.c
+++ src/main.c
@@ -1,22 +1,57 @@
+#include "defines.h"
 #include "log.h"
 #include <glad/glad.h>
-#include "gfx/window.h"
+#include <glfw/glfw3.h>
+
+/************************************************************/
+/* STATIC VARIABLES                                         */
+/************************************************************/
+
+static GLFWwindow* s_window = NULL;
+static u32 s_width = 800, s_height = 800;
+static u64 s_ticks = 0;
+
+/************************************************************/
+/* STATIC FUNCTIONS                                         */
+/************************************************************/
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    s_width = width;
+    s_height = height;
+    glViewport(0, 0, width, height);
+}
+
+/************************************************************/
+/* EXTERN FUNCTIONS                                         */
+/************************************************************/
 
 int main(void)
 {
-    struct window window = { 0 };
-    window.width = 800;
-    window.height = 800;
-    window.title = "glfw window";
-    if (!Window_Init(&window)) LOG_FATAL("Unable to initialize window.");
+    if (!glfwInit()) LOG_FATAL("Unable to initialize glfw.");
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    while (Window_Loop(&window))
+    s_window = glfwCreateWindow(s_width, s_height, "glfw window", NULL, NULL);
+    if (!s_window) LOG_FATAL("Unable to initialize glfw window.");
+
+    glfwMakeContextCurrent(s_window);
+    glfwSetFramebufferSizeCallback(s_window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        LOG_FATAL("Unable to initialize glad.");
+    glViewport(0, 0, s_width, s_height);
+
+    while (!glfwWindowShouldClose(s_window))
     {
+        glfwPollEvents();
+
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glfwSwapBuffers(window.handle);
+
+        glfwSwapBuffers(s_window);
     }
 
-    Window_Free(&window);
     return 0;
 }
